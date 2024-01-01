5229ba1ecce9a9fa0fe7613b2cd17cad4b2fd0a4
TwistzzLuffy <1060032653@qq.com>
2024-01-01 22:45:27 +0000

    Heartbeats part

--- src/raft/raft.go
+++ src/raft/raft.go
@@ -378,7 +378,7 @@ func (rf *Raft) startElection(term int) bool {
 
 		if voteNumber > len(rf.peers)/2 {
 			rf.becomeLeaderLocked()
-			go rf.replicationTicker()
+			go rf.replicationTicker(term)
 		}
 	}
 
@@ -406,6 +406,93 @@ func (rf *Raft) startElection(term int) bool {
 
 func (rf *Raft) contextCheckLocked(role Role, term int) bool {
 	return !(rf.currentTerm == term && rf.role == role)
+}
+
+/************************Replication*********/
+func (rf *Raft) AppendEntries(args *AppendEntriesArgs, reply *AppendEntriesReply) {
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+
+	reply.Term = rf.currentTerm
+	reply.Success = false
+
+	//align the term
+	if args.Term < rf.currentTerm {
+		LOG(rf.me, rf.currentTerm, DLog2, "<- S%d, Reject log", args.LeaderId)
+		return
+	}
+	if args.Term >= rf.currentTerm {
+		rf.becomeFollowerLocked(args.Term)
+	}
+	//reset the timer
+	rf.resetElectionTimeLocked()
+	reply.Success = true
+}
+
+func (rf *Raft) sendAppendEntries(server int, args *AppendEntriesArgs, reply *AppendEntriesReply) bool {
+	ok := rf.peers[server].Call("Raft.AppendEntries", args, reply)
+	return ok
+}
+
+type AppendEntriesArgs struct {
+	Term     int
+	LeaderId int
+}
+type AppendEntriesReply struct {
+	Term    int
+	Success bool
+}
+
+func (rf *Raft) replicationTicker(term int) {
+	for !rf.killed() {
+		ok := rf.startReplication(term)
+		if !ok {
+			//check the success of heartbeats
+			return
+		}
+	}
+}
+
+func (rf *Raft) startReplication(term int) bool {
+	replicationToPeer := func(peer int, args *AppendEntriesArgs) {
+		reply := &AppendEntriesReply{}
+		ok := rf.sendAppendEntries(peer, args, reply)
+
+		rf.mu.Lock()
+		defer rf.mu.Unlock()
+		if !ok {
+			LOG(rf.me, rf.currentTerm, DLog, "-> S%d, Lost or crashed", peer)
+			return
+		}
+		//align the term
+		if reply.Term > rf.currentTerm {
+			rf.becomeFollowerLocked(reply.Term)
+			return
+		}
+	}
+	rf.mu.Lock()
+	defer rf.mu.Unlock()
+	if rf.contextCheckLocked(Leader, term) {
+		LOG(rf.me, rf.currentTerm, DLeader, "Leader[T%d] -> %s[T%d]", term, rf.role, rf.currentTerm)
+		//current term is ending, and this round of heartbeat is fault
+		return false
+	}
+
+	for peer := 0; peer < len(rf.peers); peer++ {
+		if peer == rf.me {
+			//send the heartbeat to everyone, expect itself
+			continue
+		}
+
+		args := &AppendEntriesArgs{
+			Term:     term,
+			LeaderId: rf.me,
+		}
+
+		go replicationToPeer(peer, args)
+	}
+
+	return true
 }
 
 // the service or tester wants to create a Raft server. the ports
